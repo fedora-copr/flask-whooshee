bf76ade963b8f225cc3d41876fc55bc1bd1f3a1d
Bohuslav Kabrda <bkabrda@redhat.com>
2013-03-21 08:38:31 +0100

    Add test.py to archive, bump to 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except:
 
 setup(
     name = 'flask-whooshee',
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Flask - SQLAlchemy - Whoosh integration',
     long_description = 'Flask - SQLAlchemy - Whoosh integration that allows to create and search custom indexes.',
     keywords = 'flask, sqlalchemy, whoosh',
