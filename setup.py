57385023287acddb9613c66b9a079d5a16c990d0
sh4nks <peter.justin@outlook.com>
2016-11-04 10:09:11 +0100

    Update requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Flask-Whooshee
+--------------
 
-try:
-    from setuptools import setup
-except:
-    from distutils.core import setup
+Adds Whoosh support to Flask-SQLAlchemy.
 
+Links
+`````
+
+* `Documentation <https://pythonhosted.org/Flask-Whooshee/>`_
+* `Source Code <https://github.com/bkabrda/flask-whooshee>`_
+* `Issues <https://github.com/bkabrda/flask-whooshee/issues>`_
+"""
+from setuptools import setup
 
 setup(
     name='flask-whooshee',
     version='0.3.1',
-    description='Flask - SQLAlchemy - Whoosh integration',
-    long_description=''.join(open('README.md').readlines()),
+    description='Flask-SQLAlchemy - Whoosh Integration',
+    long_description=__doc__,
     keywords='flask, sqlalchemy, whoosh',
     author='Bohuslav "Slavek" Kabrda',
     author_email='bkabrda@redhat.com',
     url='https://github.com/bkabrda/flask-whooshee',
     license='BSD',
-    py_modules=['flask_whooshee', ],
-    install_requires=open('requirements.txt').read().splitlines(),
-    setup_requires=[],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Environment :: Web Environment',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-                 'Topic :: Software Development :: Libraries :: Python Modules'
-                 ]
+    py_modules=['flask_whooshee'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'blinker',
+        'Flask-Sqlalchemy',
+        'Whoosh'
+    ],
+    tests_require=[
+        'nose'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
