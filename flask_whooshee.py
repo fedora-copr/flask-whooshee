595063f9947f9880ba992f021933948d8f81ce6f
Grey Li <withlihui@gmail.com>
2017-07-17 19:37:54 +0800

    Add Unicode support

--- flask_whooshee.py
+++ flask_whooshee.py
@@ -165,6 +165,8 @@ class AbstractWhoosheer(object):
                                  ``False`` otherwise.
         """
         s = search_string.strip()
+        if not isinstance(s, unicode):
+            s = unicode(s)
         # we don't want stars from user
         s = s.replace('*', '')
         if len(s) < _get_config(cls)['search_string_min_len']:
